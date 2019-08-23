92e7db04ca9e8e48d498535b73a6df243abe2ec6
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2019-08-22 21:33:20 -0500

    Push

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+string getFirstWord(string);
+
 int main(int argc, char *argv[]) {
+    int quant;
+    string line, word;
+    map<string,int> map;
+    std::map<string,int>::iterator it;
+    getline (cin,line);
+    quant = stoi(line);
+
+    for (int i = 0; i < quant; i++) {
+        getline (cin,line);
+        word = getFirstWord(line);
+        it = map.find(word);
+        if (it == map.end())
+            map.insert( pair<string,int>(word,1));
+        else
+            map[word]++;
+    }
+    for (auto i = map.begin(); i != map.end(); i++)
+        cout << i->first << " " << i->second << endl;
+
     return 0;
 }
+
+string getFirstWord (string str){
+    string word = "";
+    for (auto x : str) {
+        if (x != ' ')
+            word += x;
+        else
+            return word;   
+    }
+}   
